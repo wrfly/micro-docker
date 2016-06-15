e1e72ce747221b9f1f5bd7e2bd16069ac061cbf4
wrfly <mr.wrfly@gmail.com>
2016-06-15 11:02:54 +0800

    chaos

--- 7ghost/_admin/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-	error_reporting(E_ALL & ~ E_NOTICE );
-	define('ADIR', str_replace("\\", "/", dirname(__FILE__)).'/');
-	require ADIR.'includes/Snoopy.class.php';
-	require ADIR.'includes/function.php';
-	
